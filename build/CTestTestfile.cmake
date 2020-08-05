ae9480e29b2fd9dd547c8dd7c27babc774a47ee3
edwardlwilson <edwardlwilson@catchandgrab.com>
2020-08-04 19:02:12 -0700

    l

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/edward/Desktop/AmazonPayDemo/react_redux_express_starter/client/amazon-pay-api-sdk-nodejs
+# Build directory: C:/Users/edward/Desktop/AmazonPayDemo/react_redux_express_starter/client/amazon-pay-api-sdk-nodejs/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
